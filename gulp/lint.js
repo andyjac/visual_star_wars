0133f19d156298b53c875d313ff2d5437b75bef0
Andrew Jacobson <andyjacuw@gmail.com>
2016-01-16 16:13:07 -0800

    Add task for linting tests

--- gulp/lint.js
+++ gulp/lint.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var paths = require('./paths');
+var helpers = require('../lib/helpers');
 
 gulp.task('lint:server', function() {
   return lint(paths.server);
@@ -12,6 +13,12 @@ gulp.task('lint:gulp', function() {
 
 gulp.task('lint:lib', function() {
   return lint(paths.lib);
+});
+
+gulp.task('lint:test', function() {
+  var testPaths = helpers.joinPaths(paths.test);
+
+  return lint(testPaths);
 });
 
 function lint(paths) {
