46209f1d7495434d7126f5682506ec6d7cb552e9
Andrew Jacobson <andyjacuw@gmail.com>
2016-01-16 16:12:40 -0800

    Add test for joinPaths

--- tests/lib/helper_test.js
+++ tests/lib/helper_test.js
@@ -44,4 +44,17 @@ describe('helper functions', function() {
     expect(value3).to.eql('1.00');
     expect(value4).to.eql('5');
   });
+
+  it('should join multiple path arrays', function() {
+    var paths = {
+      1: ['./path/to/1', './path/to/2'],
+      2: ['./path/to/3', './path/to/4'],
+      3: ['./path/to/4', './path/to/5']
+    }
+
+    var joined = helpers.joinPaths(paths);
+
+    expect(Array.isArray(joined)).to.eql(true);
+    expect(joined.length).to.eql(6);
+  });
 });
